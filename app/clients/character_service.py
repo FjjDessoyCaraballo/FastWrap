882ac2fe27d779b64856c1933c1f5ea5ae2ea6f5
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-11-20 14:31:38 +0200

    feature: adding pipeline from API to SQL injections (update and delete)

--- app/clients/character_service.py
+++ app/clients/character_service.py
@@ -1,6 +1,6 @@
 from ..models.schemas import RoleRequest
 from fastapi import HTTPException
-from .db_character import db_insertion
+from .CRUD import db_insertion, db_update, db_delete
 
 async def store_character(request: RoleRequest):
     try:
@@ -8,5 +8,14 @@ async def store_character(request: RoleRequest):
     except:
         raise
 
-# async def retrieve_character(request: RoleRequest):
-#     return
+async def update_character(request: RoleRequest):
+    try:
+        await db_update(request)
+    except:
+        raise
+
+async def delete_character(request: RoleRequest):
+    try:
+        await db_delete(request)
+    except:
+        raise
