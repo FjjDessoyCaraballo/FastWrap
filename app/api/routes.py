3116e13eb6adc23ab00108af6644ff8480c39c79
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-11-21 15:29:16 +0200

    fix: removing uuid from JSON payload to URL path for REST conformity.

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,9 @@
-from fastapi import status, APIRouter, HTTPException, Response
-from ..models.schemas import RoleRequest, ServiceRoleRequest, Completions
+from fastapi import status, Path, APIRouter, HTTPException, Response
+from ..models.schemas import RoleRequest, ServiceRoleRequest, Completions, PatchRole
 from ..clients.chat_service import store_message
-from ..clients.character_service import store_character, update_character, delete_character, get_character
+from ..clients.character_service import store_character, update_character, delete_character, get_character, get_all_character
 import logging
 import sys
-# NEED TO ADD TOKENIZED SECURITY LATER
  
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -28,41 +27,67 @@ async def characters(request: RoleRequest, response: Response):
 
     return {"message": "Character created", "data": request.dict()}
 
-@router.delete("/api/characters")
-async def characters(request: RoleRequest, response: Response):
-    http_status: int = await delete_character(request)
+@router.delete("/api/characters/{uuid}")
+async def characters(
+    request: RoleRequest,
+    response: Response,
+    uuid: str = Path(min_length=36, description="Character UUID")
+    ):
+    http_status: int = await delete_character(uuid)
     response.status_code = http_status
     logger.info(f"Received role deletion request")
 
     if http_status == status.HTTP_404_NOT_FOUND:
-        return {"message": "Character not found"}
-    
-    return {"message": "Character deleted"}
+        return {"message": "Resource not found"}
+    return {}
  
-@router.patch("/api/characters")
-async def characters(request: RoleRequest, response: Response):
+@router.patch("/api/characters/{uuid}")
+async def characters(
+    request: PatchRole,
+    response: Response,
+    uuid: str = Path(min_length=36, description="Character UUID")
+    ):
     
-    http_status: int = await update_character(request)
+    http_status: int = await update_character(uuid, request)
     response.status_code = http_status
     logger.info(f"Received role update request: {request.dict()}")
     
-    return {"message": "Character updated", "data": request.dict()}
+    return {"message": "Resource updated", "data": request.dict()}
 
-@router.get("/api/characters")
-async def characters(request: ServiceRoleRequest, response: Response):
+@router.get("/api/characters/{uuid}")
+async def characters(
+    response: Response,
+    uuid: str = Path(min_length=36, description="Character UUID")
+    ):
     
     http_status: int
     agent_role: str
 
-    agent_role, http_status = await get_character(request)
+    agent_role, http_status = await get_character(uuid)
+    response.status_code = http_status
+    
+    if http_status == status.HTTP_404_NOT_FOUND:
+        return {"message": "Resource does not exist"}
+    
+    logger.info(f"Received role fetch request: {agent_role}")
+    
+    return {"message": "Character fetched", "data": agent_role}
+
+@router.get("/api/characters")
+async def characters(response: Response):
+    
+    http_status: int
+    agent_roles: dict
+
+    agent_roles, http_status = await get_all_character()
     response.status_code = http_status
     
     if http_status == status.HTTP_404_NOT_FOUND:
         return {"message": "resource does not exist"}
     
-    logger.info(f"Received role fetch request: {request.dict()}")
+    logger.info(f"Received role fetch request: {agent_roles}")
     
-    return {"message": "Character fetched", "data": request.dict()}
+    return {"message": "Characters fetched", "data": agent_roles}
 
 @router.post("/api/chat")
 async def chat(request: Completions):
