05d3acc32f8ff98433c2158233e5076a8b631031
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-11-21 11:09:42 +0200

    feat: created ServiceRoleRequest pydantic BaseModel class for GET method of characters. In short, it's a simple and safer step to fetch a resource without adding overhead and redundancy from RoleRequest (agent_role is/should be already present).

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -3,17 +3,14 @@ from typing import Optional
 from dataclasses import dataclass
 
 class RoleRequest(BaseModel):
-    uuid: str = Field(..., min_length=1, description="Unique user ID")
-    agent_role: str = Field(..., min_length=1, description="The role of the agent")
-    TTL: Optional[int] = Field(None, gt=0, description="Time to live in seconds")
+    uuid: str = Field(..., min_length=36, description="Unique user ID.")
+    agent_role: str = Field(..., min_length=1, description="The role of the agent.")
+    TTL: Optional[int] = Field(None, gt=0, description="Time to live in seconds. Optional parameter")
 
 class Completions(BaseModel):
-    uuid: str = Field(..., min_length=1, description="Unique user ID")
-    role: str = Field(..., min_length=1, description="Role to differentiate between chatbot and user")
-    content: str = Field(..., min_length=1, description="Content of the message. Either user prompt or chatbot reply")
+    uuid: str = Field(..., min_length=36, description="Unique user ID.")
+    role: str = Field(..., min_length=1, description="Role to differentiate between chatbot and user.")
+    content: str = Field(..., min_length=1, description="Content of the message. Either user prompt or chatbot reply.")
 
-# @dataclass
-# class ResponseFormat:
-#     """Response schema for the agent."""
-#     clear_answer: str
-#     conditional_answer: str | None = None
+class ServiceRoleRequest(BaseModel):
+    uuid: str = Field(..., min_length=36, description="Unique user ID that serves as a general identifier for users and clients")
