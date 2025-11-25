03d39189368c2876d4412b9b8061f7f0a280c66c
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-11-25 13:53:19 +0200

    feat: using class for CRUD operations

--- app/clients/character_service.py
+++ app/clients/character_service.py
@@ -1,38 +1,40 @@
 from ..models.schemas import RoleRequest, ServiceRoleRequest, PatchRole
 from fastapi import HTTPException, Path
-from .CRUD import db_insertion, db_update, db_delete, db_select, db_select_all
+from .CRUD import crud_management
+
+crud = crud_management()
 
 async def store_character(request: RoleRequest) -> int:
     try:
-        http_status = await db_insertion(request)
+        http_status = await crud.db_insertion(request)
         return http_status
     except:
         raise
 
 async def update_character(uuid: str, request: PatchRole) -> int:
     try:
-        http_status = await db_update(uuid, request)
+        http_status = await crud.db_update(uuid, request)
         return http_status
     except:
         raise
 
 async def delete_character(uuid: str) -> int:
     try:
-        http_status = await db_delete(uuid)
+        http_status = await crud.db_delete(uuid)
         return http_status
     except:
         raise
 
 async def get_character(uuid: str) -> tuple[str, int]:
     try:
-        agent_role, http_status = await db_select(uuid)
+        agent_role, http_status = await crud.db_select(uuid)
         return agent_role, http_status
     except:
         raise
 
 async def get_all_character() -> tuple[str, int]:
     try:
-        agent_role, http_status = await db_select_all()
+        agent_role, http_status = await crud.db_select_all()
         return agent_role, http_status
     except:
         raise
