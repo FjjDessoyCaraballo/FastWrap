360737b00b543c5a4bc1af777fa9b9fec7845cd6
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-11-25 13:54:38 +0200

    fix: fixing store_message logic which was overwritting prompts. Next steps are to add RAG to prompts and insert langchain

--- app/clients/chat_service.py
+++ app/clients/chat_service.py
@@ -1,15 +1,22 @@
 from ..models.schemas import Completions
 from ..clients.redis_client import redis_client as r
-from fastapi import HTTPException
+from fastapi import HTTPException, status
+import logging
+import json
+ 
+logger = logging.getLogger(__name__)
 
 async def store_message(request: Completions):
     try: 
-        await r.hset(f"chat:{request.uuid}", mapping={
-            "uuid": request.uuid,
-            "role": request.role,
-            "content": request.content,
-        })
+        await r.rpush(f"chat:{request.uuid}", json.dumps({"role": request.role, "content": request.content}))
+        messages = await r.lrange(f"chat:{request.uuid}", 0, -1)
+        parsed = [json.loads(msg) for msg in messages]
+        logging.info(f"Parsed request: {parsed}")
         await r.expire(f"chat:{request.uuid}", 300)
-        return 201
+        return status.HTTP_201_CREATED
+    except HTTPException as e: # (status_code=500, detail=str(e))
+        logger.error(f"Network error caught: {e}")
+        raise
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        logger.error(f"Unexpected error caught: {e}")
+        raise 
