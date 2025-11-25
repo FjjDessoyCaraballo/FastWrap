5a8ecda52166fb4f9491baefd53fd6be7f8c1e03
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-11-25 13:52:35 +0200

    chore: fitting agent into class for modularity

--- app/agents/chatbot_agent.py
+++ app/agents/chatbot_agent.py
@@ -1,32 +1,28 @@
 import logging
 from .tools import check_products
-from ...config import settings
+from config import settings
 from langchain.agents import create_agent
 from langchain_core.language_models import BaseChatModel
 from langchain_chat_models import init_chat_model
 
 logger = logging.getLogger(__name__)
 
-# Instructions: this will come from db later
-instructions = """
-You are a store assistant that will help clients choose products from our store.
+class ChatBot():
+    def __init__(self):
+        model = detect_provider()
+        agent = create_agent(
+                model=model,
+                system_prompt=f"{instructions}",
+                tools=[check_products]
+                )
 
-YOU CANNOT place products in cart without explicit authorization from the client.
-"""
+    def _detect_provider() -> BaseChatModel:
+        try:
+            model: BaseChatModel = init_chat_model(settings.MODEL)
+            return model
+        except:
+            model: BaseChatModel = init_chat_model(settings.MODEL, model_provider=settings.MODEL_PROVIDER)
+            return model
 
-def detect_provider() -> BaseChatModel:
-    try:
-        model: BaseChatModel = init_chat_model(settings.MODEL)
-        return model
-    except:
-        model: BaseChatModel = init_chat_model(settings.MODEL, model_provider=settings.MODEL_PROVIDER)
-        return model
 
-model = detect_provider()
-
-agent = create_agent(
-        model=model,
-        system_prompt=f"{instructions}",
-        tools=[check_products]
-        )
 
