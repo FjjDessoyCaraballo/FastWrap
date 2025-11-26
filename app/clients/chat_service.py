b24cf1f4867d7e2b9b481a3dbce2a46d47b01606
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-11-26 12:32:34 +0200

    feat: chat logic implemented by saving and extracting response from client into Redis. Continuous chat logic still lacking, for now we do not have implemented a RAG logic to it, but probably will be implemented together in the ChatBot class instead of the endpoint.

--- app/clients/chat_service.py
+++ app/clients/chat_service.py
@@ -1,6 +1,8 @@
 from ..models.schemas import Completions
 from ..clients.redis_client import redis_client as r
 from fastapi import HTTPException, status
+from ..agents.chatbot_agent import ChatBot
+from typing import Any
 import logging
 import json
  
@@ -9,11 +11,18 @@ logger = logging.getLogger(__name__)
 async def store_message(request: Completions):
     try: 
         await r.rpush(f"chat:{request.uuid}", json.dumps({"role": request.role, "content": request.content}))
-        messages = await r.lrange(f"chat:{request.uuid}", 0, -1)
-        parsed = [json.loads(msg) for msg in messages]
-        logging.info(f"Parsed request: {parsed}")
+        
+        messages: list[str] = await r.lrange(f"chat:{request.uuid}", 0, -1)
+        parsed: list[dict[str, str]] = [json.loads(msg) for msg in messages]
+        
+        logging.info(f"Parsed request for {request.uuid}")
+        
+        chatbot = ChatBot()
+        response: dict[str, Any] = chatbot.chat(parsed)
+
         await r.expire(f"chat:{request.uuid}", 300)
-        return status.HTTP_201_CREATED
+        return status.HTTP_201_CREATED, response
+
     except HTTPException as e:
         logger.error(f"Network error caught: {e}")
         raise
