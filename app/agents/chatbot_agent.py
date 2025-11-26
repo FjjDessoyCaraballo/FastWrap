eca73ff1a63dc556b3a9e5f269c27595380db67e
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-11-26 12:29:56 +0200

    feat: i) Modularizing chatbot logic into class; ii) Inserting langchain to take care of different models; iii) agent.invoke takes care of chat logic with JSON exchange

--- app/agents/chatbot_agent.py
+++ app/agents/chatbot_agent.py
@@ -1,27 +1,41 @@
 import logging
-from .tools import check_products
+# from .tools import check_products
 from config import settings
 from langchain.agents import create_agent
 from langchain_core.language_models import BaseChatModel
-from langchain_chat_models import init_chat_model
+from langchain.chat_models import init_chat_model
+from typing import Any
 
 logger = logging.getLogger(__name__)
 
+instructions = "you are a helpful assistant"
+
 class ChatBot():
     def __init__(self):
-        model = detect_provider()
-        agent = create_agent(
+        model = self._detect_provider()
+        self.agent = create_agent(
                 model=model,
                 system_prompt=f"{instructions}",
-                tools=[check_products]
+                # tools=[check_products]
                 )
 
-    def _detect_provider() -> BaseChatModel:
+    def chat(self, parsed_messages: list[dict[str, str]]) -> dict[str, Any]:
+        response = self.agent.invoke({"messages": parsed_messages})
+        return response
+
+    def _detect_provider(self) -> BaseChatModel:
         try:
-            model: BaseChatModel = init_chat_model(settings.MODEL)
+            model: BaseChatModel = init_chat_model(
+                settings.MODEL,
+                api_key=settings.MODEL_KEY
+                )
             return model
         except:
-            model: BaseChatModel = init_chat_model(settings.MODEL, model_provider=settings.MODEL_PROVIDER)
+            model: BaseChatModel = init_chat_model(
+                settings.MODEL, 
+                model_provider=settings.MODEL_PROVIDER,
+                api_key=settings.MODEL_KEY
+                )
             return model
 
 
