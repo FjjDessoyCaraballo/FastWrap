42b4a041ecc8ac764f39d69c08d4f4ab3d65512d
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-12-04 13:48:34 +0200

    fix: CRUD methods now return resources that should be properly displayed to user

--- app/clients/character_service.py
+++ app/clients/character_service.py
@@ -5,35 +5,35 @@ crud = crud_management()
 
 async def store_character(request: schemas.RoleRequest) -> int:
     try:
-        http_status = await crud.db_insertion_character(request)
-        return http_status
+        character = await crud.db_insertion_character(request)
+        return character
     except:
-        raise
+        return None
 
 async def update_character(uuid: str, request: schemas.PatchRole) -> int:
     try:
-        http_status = await crud.db_update_character(uuid, request)
-        return http_status
+        updated_character = await crud.db_update_character(uuid, request)
+        return updated_character
     except:
-        raise
+        return None
 
 async def delete_character(uuid: str) -> int:
     try:
         http_status = await crud.db_delete_character(uuid)
         return http_status
     except:
-        raise
+        return None
 
 async def get_character(uuid: str) -> tuple[str, int]:
     try:
-        agent_role, http_status = await crud.db_select_character(uuid)
-        return agent_role, http_status
+        agent_role = await crud.db_select_character(uuid)
+        return agent_role
     except:
-        raise
+        return None
 
 async def get_all_character() -> tuple[str, int]:
     try:
-        agent_role, http_status = await crud.db_select_character_all()
-        return agent_role, http_status
+        agent_role = await crud.db_select_character_all()
+        return agent_role
     except:
-        raise
+        return None
