583af2cf54a8bd37a1a5418a08478ebfa9630ed2
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-12-04 13:46:24 +0200

    feat: i) moved client_service.py to appropriate directory; ii) created dependencies.py where middleware lives.

---
+++ app/auth/dependencies.py
@@ -0,0 +1,17 @@
+from fastapi import Header, HTTPException, status
+from ..clients.CRUD import crud_management
+import bcrypt
+
+crud = crud_management()
+
+async def verify_api_key(x_api_key: str = Header(...)) -> dict:
+    """
+    Dependency that verifies API key from header.
+    Returns user data if valid, raises 401 if not.
+    """
+
+    resource =  await crud.db_select_client_by_key(x_api_key)
+    if resource is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key")
+    return resource
+
