882558de85edfb1aa5dbd9f2054a49c20a529352
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-12-04 13:46:44 +0200

    feat: added extra routes for client management

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,8 +1,9 @@
-from fastapi import status, Path, APIRouter, Response, Header
+from fastapi import status, Path, APIRouter, Response, Header, HTTPException
 from ..models import schemas
 from ..clients.chat_service import store_message
-from ..auth import client_service as auth
+from ..clients import client_service
 from ..clients import character_service
+from ..auth import verify_api_key
 from typing import Any
 from uuid import UUID
 import logging
@@ -11,47 +12,52 @@ import sys
 router = APIRouter()
 logger = logging.getLogger(__name__)
   
-@router.get("/") 
-async def root(status_code=status.HTTP_200_OK): 
+@router.get("/", status_code=status.HTTP_200_OK) 
+async def root(): 
     
     logger.info("Received request at root") 
     
-    return ({ "Title": "VanaciPrime Chatbot Wrapper",
-             "Version": "1.0.0.",
-             "Author": "Felipe",
-             "Status": "Development"})
+    return ({ 
+        "Title": "VanaciPrime Chatbot Wrapper",
+        "Version": "1.0.0.",
+        "Author": "Felipe",
+        "Status": "Development"
+        })
  
 @router.post("/api/characters")
-async def characters(request: schemas.RoleRequest, response: Response):
+async def characters(
+    request: schemas.RoleRequest,
+    user = Depends(verify_api_key) 
+    ):
     
-    http_status: int = await character_service.store_character(request)
-    response.status_code = http_status
-    logger.info(f"Received role creation request. Data: {request.dict()}")
+    resource: dict = await character_service.store_character(request)
+    if resource is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Character not found")
+    logger.info("Received role creation request.")
 
     return {
         "message": "Character created",
-        "data": request.dict()
+        "data": resource
         }
 
-@router.delete("/api/characters/{uuid}")
+@router.delete("/api/characters/{uuid}", status_code=status.HTTP_204_NO_CONTENT)
 async def characters(
     request: schemas.RoleRequest,
-    response: Response,
+    user = Depends(verify_api_key),
     uuid: str = Path(min_length=36, description="Character UUID")
     ):
     http_status: int = await character_service.delete_character(uuid)
-    response.status_code = http_status
     logger.info(f"Received role deletion request")
 
     if http_status == status.HTTP_404_NOT_FOUND:
-        return {"message": "Resource not found"}
-    return {}
+        raise HTTPException(status_code=404, detail="Resource not found")
  
-@router.patch("/api/characters/{uuid}")
+@router.patch("/api/characters/{uuid}", status_code=status.HTTP_201_CREATED)
 async def characters(
     request: schemas.PatchRole,
     response: Response,
-    uuid: str = Path(min_length=36, description="Character UUID")
+    uuid: str = Path(min_length=36, description="Character UUID"),
+    user = Depends(verify_api_key)
     ):
     
     http_status: int = await character_service.update_character(uuid, request)
@@ -63,39 +69,37 @@ async def characters(
         "data": request.dict()
         }
 
-@router.get("/api/characters/{uuid}")
+@router.get("/api/characters/{uuid}", status_code=status.HTTP_200_OK)
 async def characters(
-    response: Response,
-    uuid: str = Path(min_length=36, description="Character UUID")
+    uuid: str = Path(min_length=36, description="Character UUID"),
+    user = Depends(verify_api_key)
     ):
-    
-    http_status: int
+
     agent_role: str
 
-    agent_role, http_status = await character_service.get_character(uuid)
-    response.status_code = http_status
+    agent_role = await character_service.get_character(uuid)
+
+    if agent_role is None:
+        raise HTTPException(status_code=404, detail="Resource not found")
     
-    if http_status == status.HTTP_404_NOT_FOUND:
-        return {"message": "Resource does not exist"}
-    
-    logger.info(f"Received role fetch request: {agent_role}")
+    logger.info(f"Received role fetch request")
     
     return {
         "message": "Character fetched",
         "data": agent_role
         }
 
-@router.get("/api/characters")
-async def characters(response: Response):
+@router.get("/api/characters", status_code=status.HTTP_200_OK)
+async def characters(
+    user = Depends(verify_api_key)
+):
 
-    http_status: int
     agent_roles: dict
 
-    agent_roles, http_status = await character_service.get_all_character()
-    response.status_code = http_status
+    agent_roles = await character_service.get_all_character()
     
-    if http_status == status.HTTP_404_NOT_FOUND:
-        return {"message": "resource does not exist"}
+    if agent_role is None:
+        raise HTTPException(status_code=404, detail="Resource not found")
     
     logger.info(f"Received role fetch request: {agent_roles}")
     
@@ -104,11 +108,12 @@ async def characters(response: Response):
         "data": agent_roles
         }
 
-@router.post("/api/chat")
+@router.post("/api/chat", status_code=status.HTTP_201_CREATED)
 async def chat(
     response: Response, 
     request: schemas.Completions,
-    x_store_id: str = Header(..., min_length=36, max_length=36)
+    x_store_id: str = Header(..., min_length=36, max_length=36),
+    user = Depends(verify_api_key)
     ):
     """
     This endpoint utilizes OpenAI API Completions pattern establishing user and content.
@@ -123,55 +128,64 @@ async def chat(
     http_status: int 
     prompt: dict[str, Any]
     character_uuid: str = x_store_id
-    logger.debug(f"header: {x_store_id}")
-    http_status, prompt = await store_message(request, character_uuid)
-    response.status_code = http_status
+    http_status, prompt = await store_message(request, character_uuid, api_key)
+
+    if prompt is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detai="Not found")
 
     return {
         "message": "Chat request posted",
         "data": prompt
         }
 
-@router.get("/health")
-async def health(status_code=status.HTTP_200_OK):
+@router.get("/health", status_code=status.HTTP_200_OK)
+async def health():
     return {"Health": "OK"}
 
-@router.post("/auth/signup")
+@router.post("/auth/signup", status_code=status.HTTP_201_CREATED)
 async def signup(
-    response: Response,
-    request: schemas.SignupRequest
+    request: schemas.AuthRequest
 ):
-    http_status: int
     api_key: str
 
-    api_key, http_status = await auth.register_client(request)
+    api_key, resource = await client_service.register_client(request)
 
-    if http_status != status.HTTP_201_CREATED:
-        return {
-            "message": "Error during signup",
-            "status": 401
-        }
+    if api_key is None or resource is None:
+        raise HTTPException(status_code=400, detail="Failed to create account")
     return {
         "message": "Signup request received",
         "api_key": api_key,
-        "status": http_status
+        "data": resource
         }
 
-# @router.post("/auth/login")
-# async def login(
-#     response: Response,
-#     request: schemas.LoginRequest,
-#     x_api_key: str = Header(..., min_length=36, max_length=36)
-# ):
-#     api_key: str = x_api_key
-#     http_status: int = await auth.login_client(request, api_key)
+@router.get("/api/clients/me", status_code=status.HTTP_200_OK)
+async def clients(
+    request: schemas.AuthRequest,
+    ):
+
+    api_key: str = x_api_key
+
+    resource: tuple[str, str] = await client_service.get_client(request)
+
+    if resource is None:
+        raise HTTPException(status_code=404, detail="Resource not found")
     
-#     if http_status != status.HTTP_200_OK:
-#         return {
-#             "message": "Unauthorized",
-#             "status": 401
-#         }
+    logger.info(f"Received role fetch request")
     
-#     return {
-#         "message": 
-#     }
+    return {
+        "message": "Character fetched",
+        "data": resource
+        }
+
+@router.delete("/clients/me", status_code=status.HTTP_204_NO_CONTENT)
+async def clients(
+    request: schemas.AuthRequest,
+):
+    http_status: int = await client_service.delete_client(request, api_key)
+    
+    if http_status is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+    
+    return {
+        "message": "Resource deleted"
+    }
