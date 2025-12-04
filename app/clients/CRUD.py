ed9ba74f4b703d6b98aeba28ad61ae7189a041cc
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-12-04 13:47:58 +0200

    feat: added extra CRUD management for clients and API key holders

--- app/clients/CRUD.py
+++ app/clients/CRUD.py
@@ -13,64 +13,93 @@ class crud_management():
     def __init__(self):
         self.DB_PATH = PROJECT_ROOT / "database/fastwrapper.db"
 
-    async def db_insertion_character(self, request: schemas.RoleRequest) -> int:
+    async def db_insertion_character(self, request: schemas.RoleRequest) -> tuple[str] | None:
         logger.info(f"Inserting new character")
         try:
             async with aiosqlite.connect(self.DB_PATH) as conn:
 
                 if request.TTL is None:
-                    await conn.execute(f"""INSERT INTO characters ( uuid, agent_role ) 
+                    cursor = await conn.execute("""
+                    INSERT INTO characters ( uuid, agent_role ) 
                     VALUES (?, ?)
+                    RETURNING *
                     """, (request.uuid, request.agent_role))
                 else:
-                    await conn.execute(f"""INSERT INTO characters ( uuid, agent_role, TTL ) 
+                    cursor = await conn.execute("""
+                    INSERT INTO characters ( uuid, agent_role, TTL ) 
                     VALUES ( ?, ?, ? )
+                    RETURNING *
                     """, (request.uuid, request.agent_role, request.TTL))
+
+                row = cursor.fetchone()
+
+                if row is None:
+                    logger.warning("Resource could not be created.")
+                    return None
 
                 await conn.commit()
 
                 logger.info("Successfully created ID with new role")
 
-                return status.HTTP_201_CREATED
+                return row
 
         except aiosqlite.DatabaseError as e:
             logger.error(f"Database error: {e}")
-            raise
+            return None
         except Exception as e:
             logger.error(f"Unexpected error: {e}")
-            raise
+            return None
 
-    async def db_update_character(self, uuid: str, request: schemas.RoleRequest) -> int:
+    async def db_update_character(self, uuid: str, request: schemas.RoleRequest) -> tuple[str] | None:
         logger.info("Updating new role")
         try:
             async with aiosqlite.connect(self.DB_PATH) as conn:
         
                 if request.TTL is None:
-                    await conn.execute("UPDATE OR ABORT characters SET agent_role = ? WHERE uuid = ?", 
+                    cursor = await conn.execute('''
+                    UPDATE OR ABORT characters
+                    SET agent_role = ?
+                    WHERE uuid = ?
+                    RETURNING *
+                    ''', 
                     (request.agent_role, uuid))
                 else:
-                    await conn.execute("UPDATE OR ABORT characters SET agent_role = ?, TTL = ? WHERE uuid = ?", 
+                    cursor = await conn.execute('''
+                    UPDATE OR ABORT characters 
+                    SET agent_role = ?, TTL = ? 
+                    WHERE uuid = ?
+                    RETURNING *
+                    ''', 
                     (request.agent_role, request.TTL, uuid))
-        
+
+                row = await cursor.fetchone()
+
+                if row is None:
+                    logger.warning("Update failed")
+                    return None
+
                 await conn.commit()
         
                 logger.info(f"Successfully update ID with new role")
         
-                return status.HTTP_200_OK
+                return row
         
         except aiosqlite.DatabaseError as e:
             logger.error(f"Database error: {e}")
-            raise
+            return None
         except Exception as e:
             logger.error(f"Unexpected error: {e}")
-            raise
+            return None
 
-    async def db_delete_character(self, uuid: str) -> int:
+    async def db_delete_character(self, uuid: str) -> tuple[str] | None:
         logger.info(f"Deleting role for ID: {uuid}")
         try:
             async with aiosqlite.connect(self.DB_PATH) as conn:
         
-                cursor = await conn.execute("DELETE FROM characters WHERE uuid = ?", 
+                cursor = await conn.execute('''
+                DELETE FROM characters 
+                WHERE uuid = ?
+                ''', 
                 (uuid,))
         
                 if cursor.rowcount == 0:
@@ -86,116 +115,200 @@ class crud_management():
         
         except aiosqlite.DatabaseError as e:
             logger.error(f"Database error: {e}")
-            raise
+            return None
         except Exception as e:
             logger.error(f"Unexpected error: {e}")
-            raise
+            return None
 
-    async def db_select_character(self, uuid: str) -> tuple[str, int]:
-        logger.info(f"Fetching information for role ID: {uuid}")
+    async def db_select_character(self, uuid: str) -> tuple[str] | None:
+        logger.info(f"Fetching role information")
         try:
             async with aiosqlite.connect(self.DB_PATH) as conn:
         
-                cursor = await conn.execute("SELECT agent_role FROM characters WHERE uuid = ?", (uuid,))
+                cursor = await conn.execute('''
+                SELECT agent_role 
+                FROM characters 
+                WHERE uuid = ?
+                ''', (uuid,))
                 row = await cursor.fetchone()
         
                 if row is None:
                     logger.warning("uuid does not correspond to any existing IDs in database")
-                    return None, status.HTTP_404_NOT_FOUND
+                    return None
         
                 agent_role = row[0]
         
-                logger.info(f"Successfully fetched data {agent_role}")
+                logger.info(f"Successfully fetched data")
         
-                return agent_role, status.HTTP_200_OK
+                return row
         
         except aiosqlite.DatabaseError as e:
             logger.error(f"Database error: {e}")
-            raise
+            return None
         except Exception as e:
             logger.error(f"Unexpected error: {e}")
-            raise
+            return None
 
-    async def db_select_character_all(self) -> tuple[list[tuple] | None, int]:
+    async def db_select_character_all(self) -> tuple[list[tuple] | None]:
         try:
             async with aiosqlite.connect(self.DB_PATH) as conn:
         
-                cursor = await conn.execute("SELECT * FROM characters")
+                cursor = await conn.execute('''
+                SELECT * 
+                FROM characters
+                ''')
+                
                 rows = await cursor.fetchall()
         
                 if rows is None:
                     logger.warning("Database is empty")
-                    return None, status.HTTP_404_NOT_FOUND
+                    return None
         
                 logger.info("retrieving all characters information")
         
-                return rows, status.HTTP_200_OK
+                return rows
         
         except aiosqlite.DatabaseError as e:
             logger.error(f"Database error: {e}")
-            raise
+            return None
         except Exception as e:
             logger.error(f"Unexpected error: {e}")
-            raise
+            return None
 
-    async def db_insert_client(self, email: str, password: str):
+    async def db_insert_client(self, email: str, password: str) -> tuple[str] | None:
         try:
             hashed_password: str = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode('utf-8')
             api_key: str = f"vcp_{secrets.token_urlsafe(32)}"
-            hashed_key: str = bcrypt.hashpw(api_key.encode(), bcrypt.gensalt()).decode('utf-8')
             async with aiosqlite.connect(self.DB_PATH) as conn:
                 
-                await conn.execute('''
-                    INSERT INTO clients ( email, password, api_key )
-                    VALUES ( ? , ? , ? )
-                ''', (email, hashed_password, hashed_key))
+                cursor = await conn.execute('''
+                INSERT INTO clients ( email, password, api_key )
+                VALUES ( ? , ? , ? )
+                RETURNING *
+                ''', (email, hashed_password, api_key))
                 
+                resource = await cursor.fetchone()
+
+                if resource is None:
+                    logger.warning("Failed to create resource")
+                    return None, None
+
                 logger.debug('user registered')
                 
                 await conn.commit()
                 
-                return api_key, status.HTTP_201_CREATED
+                return api_key, resource
 
         except aiosqlite.IntegrityError:
             logger.warning(f'Email already exists in database: {email}')
-            return None, status.HTTP_409_CONFLICT
+            return None
         except aiosqlite.DatabaseError as e:
             logger.error(f'Database error: {e}')
-            raise
+            return None
         except Exception as e:
             logger.error(f'Unexpected error: {e}')
-            raise
+            return None
 
-    async def db_select_client(self, email: str, password: str, api_key: str):
+    async def db_select_client(self, email: str, password: str):
         try:
             async with aiosqlite.connect(self.DB_PATH) as conn:
                 cursor = await conn.execute('''
-                    SELECT id, password, api_key FROM clients WHERE email = ?
+                SELECT id, password 
+                FROM clients 
+                WHERE email = ?
                 ''', (email,))
 
                 row = await cursor.fetchone()
 
                 if row is None:
                     logger.warning("No matching email in attempt")
-                    return status.HTTP_401_UNAUTHORIZED
+                    return None
 
                 stored_pw_hash = row[1]
-                stored_api_hash = row[2]
 
                 if not bcrypt.checkpw(password.encode(), stored_pw_hash.encode()):
                     logger.warning("Wrong password attempt")
-                    return status.HTTP_401_UNAUTHORIZED
-                elif not bcrypt.checkpw(api_key.encode(), stored_api_hash.encode()):
-                    logger.warning("Wrong api key attempt")
-                    return status.HTTP_401_UNAUTHORIZED
+                    return None
                 
                 logger.info("User properly identified")
                 
-                return status.HTTP_200_OK
+                return row
 
         except aiosqlite.DatabaseError as e:
             logger.error(f'Database error: {e}')
-            raise
+            return None
         except Exception as e:
             logger.error(f'Unexpected error: {e}')
-            raise
+            return None
+
+
+    async def db_select_client_by_key(self, api_key: str) -> dict | None:
+        try:
+            async with aiosqlite.connect(self.DB_PATH) as conn:
+                cursor = await conn.execute('''
+                SELECT id, email, api_key
+                FROM clients 
+                WHERE api_key = ?
+                ''', (api_key,))
+
+                resource = await cursor.fetchone()
+
+                if resource is None:
+                    logger.warning("No matching key in attempt")
+                    return None
+                
+                logger.info("API key properly identified")
+                
+                return resource
+
+        except aiosqlite.DatabaseError as e:
+            logger.error(f'Database error: {e}')
+            return None
+        except Exception as e:
+            logger.error(f'Unexpected error: {e}')
+            return None
+
+    async def db_delete_client(self, email: str, password: str) -> int | None:
+        logger.info(f"Deleting client account")
+        try:
+            async with aiosqlite.connect(self.DB_PATH) as conn:
+                
+                cursor = await conn.execute('''
+                SELECT id, password 
+                FROM clients
+                WHERE email = ?
+                ''', (email,))
+
+                row = await cursor.fetchone()
+
+                if row is None:
+                    logger.warning("Email not found")
+                    return None
+
+                stored_pw_hash = row[1]
+                if not bcrypt.checkpw(password.encode(), stored_pw_hash.encode()):
+                    logger.warning("Wrong password attempt")
+                    return None  
+
+                cursor = await conn.execute('''
+                DELETE FROM clients 
+                WHERE email = ?
+                ''', 
+                (email,))
+
+                if cursor.rowcount == 0:
+                    logger.warning("Email was deleted by another process")
+                    return None
+
+                http_status = status.HTTP_204_NO_CONTENT
+        
+                await conn.commit()
+        
+                return http_status
+        
+        except aiosqlite.DatabaseError as e:
+            logger.error(f"Database error: {e}")
+            return None
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+            return None
