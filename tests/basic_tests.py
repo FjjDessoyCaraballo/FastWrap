3334ee48300ada0228372a0ee1e81f18ad623079
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-01-12 16:43:26 +0200

    feat: adding class MockUser to keep data during user creation test

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -1,16 +1,43 @@
 import pytest
+import random
+import string
 from fastapi.testclient import TestClient
 from main import app
+import logging
 
 client = TestClient(app)
+
+class MockUser():
+    def __init__(self):
+        self.email: str = self.generate_random_email()
+        self.password: str = self.generate_random_password()
+        self.api_key: str = ''
+
+    def generate_random_email(self) -> str:
+        random_string: str = ''.join(random.choices(string.ascii_letters, k=10))
+        random_email: str = random_string + "@example.com"
+        return random_email
+
+    def generate_random_password(self) -> str:
+        random_lower_string: str = ''.join(random.choices(string.ascii_lowercase, k=3))
+        random_upper_string: str = ''.join(random.choices(string.ascii_uppercase, k=3))
+        random_number: str = ''.join(random.choices(string.digits, k=4))
+        special_case: str = "!"
+
+        random_password: str = random_lower_string + random_upper_string + random_number + special_case
+
+        return random_password
+
+test_user = MockUser()
 
 def test_signup_success():
     response = client.post(
         "/auth/signup",
         json={
-            "email": "test@example.com",
-            "password": "ValidPass123!"
+            "email": test_user.email,
+            "password": test_user.password
         }
     )
+    test_user.api_key = response.json()["data"]["api_key"]
     assert response.status_code == 201
     assert "api_key" in response.json()["data"]
