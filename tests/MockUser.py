39978bfef892b878c2ce8466c5948aaecb92d0fd
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-01-21 15:55:31 +0200

    feat: modularizing MockUser into its own file

---
+++ tests/MockUser.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+class MockUser():
+    def __init__(self):
+        self.email: str = self.generate_random_email()
+        self.password: str = self.generate_random_password()
+        self.api_key: str = ''
+
+    def generate_random_email(self) -> str:
+        random_string: str = ''.join(random.choices(string.ascii_letters, k=10))
+        random_email: str = random_string + "@example.com"
+        return random_email
+
+    def generate_random_password(self) -> str:
+        random_lower_string: str = ''.join(random.choices(string.ascii_lowercase, k=3))
+        random_upper_string: str = ''.join(random.choices(string.ascii_uppercase, k=3))
+        random_number: str = ''.join(random.choices(string.digits, k=4))
+        special_case: str = "!"
+
+        random_password: str = random_lower_string + random_upper_string + random_number + special_case
+
+        return random_password
+
