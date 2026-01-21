0761b3c5af091b30873850e3965f707edb6a143b
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-01-21 16:06:19 +0200

    chore: adding mock standard values for development

--- config.py
+++ config.py
@@ -3,27 +3,27 @@ from typing import Optional
 from pydantic import ConfigDict
 
 class Settings(BaseSettings):
-    FASTWRAP_API_KEY: str
+    FASTWRAP_API_KEY: str = 1234
     REDIS_API_KEY: str
-    REDIS_USER: str
+    REDIS_USER: str = "User123"
     REDIS_USER_PW: str
-    REDIS_HOST: str
-    REDIS_PORT: int
-    PORT: int
-    HOST: str
-    MODEL: str
+    REDIS_HOST: str = "redis"
+    REDIS_PORT: int = 6379
+    PORT: int = 8555
+    HOST: str = "0.0.0.0"
+    MODEL: str = "gpt-4o-mini"
     MODEL_KEY: str
     MODEL_PROVIDER: str | None = None
     LANGCHAIN_API_KEY: str | None = None
     LANGSMITH_TRACING_V2: bool = True
-    model_config = ConfigDict(env_file=".env")
-    POSTGRES_HOST: str
+    POSTGRES_HOST: str = "postgres"
     POSTGRES_PORT: int = 5432
-    POSTGRES_DB: str
+    POSTGRES_DB: str = "fastwrap_db"
     POSTGRES_USER: str
-    POSTGRES_PW: str
+    POSTGRES_PW: str 
     DATABASE_URL: str
     EMBEDDING_MODEL: str = 'text-embedding-3-small'
     EMBEDDING_DIM: int = 1536
+    model_config = ConfigDict(env_file=".env")
 
 settings = Settings()
