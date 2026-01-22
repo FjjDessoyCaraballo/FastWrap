ac36309e9af13c62f135a49d23d72eae7df4c468
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-01-22 11:51:33 +0200

    fix: fixes all over tests [wrong referencing in data payload, unnecessary trailing slash, typos, etc]

--- tests/character_test.py
+++ tests/character_test.py
@@ -3,6 +3,7 @@ import asyncio
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from main import app
+from .MockUser import MockUser
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_character_creation_with_ttl(authenticated_user: MockUser):
@@ -12,17 +13,17 @@ async def test_character_creation_with_ttl(authenticated_user: MockUser):
             headers={ "x-api-key": authenticated_user.api_key },
             json={
                 "agent_role": "You are a standup comedian.",
-                "TTL": 1000
+                "ttl": 1000
             }
         )
 
     response_data = response.json()
 
-    assert status_code == 201
+    assert response.status_code == 201
     assert "data" in response_data, f"'data' field is missing from payload: {response_data}"
     assert "agent_role" in response_data["data"], f"'agent_role' field is missing from payload: {response_data}"
     assert "id" in response_data["data"], f"'id' field is missing from payload: {response_data}"
-    assert "TTL" in response_data["data"], f"'TTL' field is missing from payload: {response_data}"
+    assert "ttl" in response_data["data"], f"'TTL' field is missing from payload: {response_data}"
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_character_creation_no_ttl(authenticated_user: MockUser):
@@ -34,10 +35,10 @@ async def test_character_creation_no_ttl(authenticated_user: MockUser):
         )
 
     response_data = response.json()
-
+    print(response_data)
     authenticated_user.character_id = response_data["data"]["id"]
 
-    assert status_code == 201
+    assert response.status_code == 201
     assert "data" in response_data, f"'data' field is missing from payload: {response_data}"
     assert "agent_role" in response_data["data"], f"'agent_role' field is missing from payload: {response_data}"
     assert response_data["data"]["agent_role"] == authenticated_user.character
@@ -45,38 +46,34 @@ async def test_character_creation_no_ttl(authenticated_user: MockUser):
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_character_get(authenticated_user: MockUser):
-    with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get(
             f"/api/characters/{authenticated_user.character_id}",
             headers={ "x-api-key": authenticated_user.api_key }
         )
 
-    reponse_data = response.json()
+    response_data = response.json()
 
     assert response.status_code == 200, f"Expected HTTP code 200, got {response.status_code} instead."
-    assert "data" in reponse_data, f"'data' is missing from payload."
-    assert "agent_role" in reponse_data["data"], f"'agent_role' is missing from payload."
-    assert response_data["data"]["agent_role"] == authenticated_user.character
+    assert "data" in response_data, f"'data' is missing from payload: {response_data}"
+    assert response_data["data"] == authenticated_user.character
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_character_get_all(authenticated_user: MockUser):
-    with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get(
-            "/api/characters/",
+            "/api/characters",
             headers={ "x-api-key": authenticated_user.api_key }
         )
 
-    reponse_data = response.json()
+    response_data = response.json()
 
     assert response.status_code == 200, f"Expected HTTP code 200, got {response.status_code} instead."
-    assert "data" in reponse_data, f"'data' is missing from payload."
-    assert "agent_role" in reponse_data["data"], f"'agent_role' is missing from payload."
-    assert response_data["data"]["agent_role"] == authenticated_user.character
-
+    assert "data" in response_data, f"'data' missing from payload: {response_data}"
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_character_patch(authenticated_user: MockUser):
-    with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.patch(
             f"/api/characters/{authenticated_user.character_id}",
             headers={ "x-api-key": authenticated_user.api_key },
@@ -86,13 +83,13 @@ async def test_character_patch(authenticated_user: MockUser):
         assert response.status_code == 201
         response_data = response.json()
         assert "data" in response_data, f"'data' field missing in payload."
-        assert "agent_role" in response_data["data"], f"'agent_role', missing in data dicitonary in payload."
-        assert response_data["data"]["agent_role"] != authenticated_user.character
-        authenticated_user.character = response_data["data"]["agent_role"]
+        print(response_data["data"])
+        assert response_data["data"] != authenticated_user.character, f"Payload in wrong format: {response_data}"
+        authenticated_user.character = response_data["data"]
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_character_deletion(authenticated_user: MockUser):
-    with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.delete(
             f"/api/characters/{authenticated_user.character_id}",
             headers={ "x-api-key": authenticated_user.api_key }
