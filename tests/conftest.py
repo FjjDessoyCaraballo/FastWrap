6b6dd6fe692c6536e79cbc2f7e05d795c2dc3cf2
Felipe Dessoy Caraballo <63795029+FjjDessoyCaraballo@users.noreply.github.com>
2026-01-29 11:58:33 +0200

    test: testing workflow automation in gh with new yaml file (#13)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,19 @@
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
+from app.database.init import init_db, close_db
 import pytest
 import asyncio
 from main import app
 from .MockUser import MockUser
 
-@pytest_asyncio.fixture(scope="module", loop_scope="module")
+@pytest_asyncio.fixture(scope="session", loop_scope="session", autouse=True)
+async def setup_database():
+    """Initialization of database pool once for all tests, closes on teardown"""
+    await init_db()
+    yield
+    await close_db()
+
+@pytest_asyncio.fixture(scope="module", loop_scope="session")
 async def authenticated_user():
     test_user = MockUser()
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
