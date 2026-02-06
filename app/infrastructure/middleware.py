5b5f5d939942dd49d4fcaecf1c8698d4e1f6f03d
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-02-06 11:41:17 +0200

    feat: inserted fixed window logic to middleware

--- app/infrastructure/middleware.py
+++ app/infrastructure/middleware.py
@@ -2,16 +2,19 @@ from .redis_client import redis_client as r
 from fastapi import status
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 
 class RateLimitMiddleware(BaseHTTPMiddleware):
     async def dispatch(
         self,
-        request,
-        call_next
-        ):
+        request: Request,
+        call_next: Callable[[Request], Awaitable[Response]]
+        ) -> Response:
         """
         Rate limiter to avoid DDoS attacks. This middleware is responsible for checking
         every endpoint at each and every time they are used. The algorithm used here is
@@ -23,25 +26,48 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
 
         Parameters:
         -----------
-        x_forwarded_for : str
-            header element containing IP of client
+        request : Request
+            HTTP request coming from an user.
+
+        call_next : Callable[[Request], Awaitable[Response]]
+            Complex type defined by base class BaseHTTPMiddleware
 
         Returns:
         -------- 
-        
-        """
-        
+        response : Response
+            HTTP response sent to user.
+        """        
         ip = request.headers.get("x-forwarded-for")
         if ip is None:
+            logger.debug("x-forwarded-for not present in headers")
             if request.client is not None:
+                logger.debug("fallback to request.client.host")
+                # fallback in case headers do not contain IP
                 ip = request.client.host
-            else: 
+            else:
+                logger.error("No IP present in headers, sending JSON with HTTP code 400")
                 return JSONResponse(
                     status_code=400,
                     content={"detail": "IP not present in headers"}
                     )
 
-        logger.info(f"ip: {ip}")
-        
-        response = await call_next(request)
+        # At this point, we are certain to have an IP
+        logger.info(f"ip: {ip}") # Check logs
+
+        try:
+            now = int(time.time())
+            bucket = now // 10
+
+            attempts: int = await r.incr(f"middleware:{ip}:{bucket}")
+            if attempts == 1:
+                await r.expire(f"middleware:{ip}:{bucket}", 15)
+            elif attempts >= 3:
+                return JSONResponse(
+                    status_code=429,
+                    content={"detail": "Too many requests"}
+                )
+        except Exception as e:
+            logger.error(f"Unexpected error with Redis: {e}")
+
+        response: Response = await call_next(request)
         return response
