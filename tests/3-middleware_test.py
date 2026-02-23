d47db3ab15a38c17e6162867e08013521c6050dc
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-02-23 11:56:56 +0200

    feat: making middleware test scalable by removing hardcoded values

--- tests/3-middleware_test.py
+++ tests/3-middleware_test.py
@@ -2,16 +2,17 @@ import pytest
 import asyncio
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
+from config import settings
 from main import app
 import time
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_middleware_limit():
-    time.sleep(15) # wait for the window to refresh
+    time.sleep(settings.API_WINDOW) # wait for the window to refresh
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        for i in range(100):
+        for i in range(settings.API_LIMIT):
             response = await ac.get("/")
             assert response.status_code == 200, f"Request {i + 1} supposed to succeed"
         response = await ac.get("/")
         assert response.status_code == 429, f"Expected 429, got {response.status_code}"
-        time.sleep(11)
+        time.sleep(settings.API_WINDOW)
